8b2ea17c0fc35ab9b17254785827668ed72336df
cuneyt <cuneytky@gmail.com>
2023-09-01 09:52:34 +0300

    added solutions

---
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/CartController.java
@@ -0,0 +1,12 @@
+package com.cydeo.lab04springmvc.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class CartController {
+
+
+
+}
